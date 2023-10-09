528da7d05fd77737d06054123689c176383fd41b
Breezy Frost <breezyfrost@163.com>
2023-10-10 00:54:42 +0800

    feat: add mock

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import axios from 'axios';
+  import { Message } from '@arco-design/web-vue';
+
+  const login = () => {
+    axios({
+      url: '/api/user/login',
+      method: 'post',
+      data: {
+        username: 'admin',
+        password: 'admin',
+      },
+    }).then((response) => {
+      if (response.data.code === 20000) {
+        Message.success('login success');
+      } else {
+        Message.error('login fail');
+      }
+    });
+  };
+</script>
 
 <template>
-  <div> <h1>Hello Vue</h1></div>
+  <div>
+    <h1>Hello Vue</h1>
+  </div>
+  <a-button type="primary" @click="login">Login</a-button>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import App from './App.vue';
 import '@/assets/style/global.less';
+import './mock';
 
 const app = createApp(App);
 
