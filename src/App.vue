c04bae826c2d6c72f4aaf846bc7db819ad5fbb29
Breezy Frost <breezyfrost@163.com>
2023-10-11 01:03:36 +0800

    feat: add login animation

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,5 @@
-<script setup lang="ts">
-  import axios from 'axios';
-  import { Message } from '@arco-design/web-vue';
-
-  const login = () => {
-    axios({
-      url: '/api/user/login',
-      method: 'post',
-      data: {
-        username: 'admin',
-        password: 'admin',
-      },
-    }).then((response) => {
-      if (response.data.code === 20000) {
-        Message.success('login success');
-      } else {
-        Message.error('login fail');
-      }
-    });
-  };
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Hello Vue</h1>
-  </div>
-  <a-button type="primary" @click="login">Login</a-button>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <router-view />
 </template>
-
-<style scoped lang="less">
-  div {
-    h1 {
-      color: @color;
-    }
-  }
-</style>
