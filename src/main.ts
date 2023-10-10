98b02fb307e3fc30c8637a6c436df2f7494cf757
Breezy Frost <breezyfrost@163.com>
2023-10-11 01:04:25 +0800

    feat: add login animation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from 'vue';
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
-import App from './App.vue';
+import router from './router';
 import '@/assets/style/global.less';
 import './mock';
+import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(ArcoVue, {});
 app.use(ArcoVueIcon);
+app.use(router);
 
 app.mount('#app');
---
+++ src/views/login/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div :class="{ 'container': true, 'sign-up-mode': isActive }">
+    <div class="image-container">
+      <div class="inner-left">
+        <a-button type="primary" shape="round" @click="onClick">登入</a-button>
+        <LoginSvg />
+      </div>
+      <div class="inner-right">
+        <a-button type="primary" shape="round" @click="onClick">注册</a-button>
+        <RegisterSvg />
+      </div>
+    </div>
+    <div class="login-container"> </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import LoginSvg from '@/assets/images/login.svg';
+  import RegisterSvg from '@/assets/images/register.svg';
+
+  const isActive = ref<boolean>(false);
+
+  const onClick = () => {
+    isActive.value = !isActive.value;
+  };
+</script>
+
+<style lang="less" scoped>
+  .container {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  .container::before {
+    position: absolute;
+    width: 200vh;
+    height: 200vh;
+    background-image: linear-gradient(-45deg, #6266f5 0%, #04befe 100%);
+    border-radius: 50%;
+    transform: translate(-50%, -55%);
+    transition: 1.8s ease-in-out;
+    content: '';
+  }
+
+  .container.sign-up-mode::before {
+    transform: translate(50%, -55%);
+  }
+
+  .image-container {
+    display: flex;
+    height: 100%;
+  }
+
+  .inner-left {
+    z-index: 1;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-around;
+    padding: 3rem 5% 2rem 15%;
+  }
+
+  .inner-right {
+    z-index: 1;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-around;
+    padding: 3rem 15% 2rem 5%;
+  }
+
+  svg {
+    width: 100%;
+  }
+
+  .login-container {
+    position: absolute;
+    top: 20%;
+    right: 0;
+    width: 50%;
+    height: 50%;
+    background-color: rgb(250 215 219);
+    transition: 1s ease-in-out;
+    transition-delay: 500ms;
+  }
+
+  .container .inner-right {
+    transform: translate(100%);
+    transition: 1s ease-in-out;
+    transition-delay: 500ms;
+  }
+
+  .container.sign-up-mode .inner-right {
+    transform: translate(0%);
+    transition: 1s ease-in-out;
+    transition-delay: 500ms;
+  }
+
+  .container .inner-left {
+    transform: translate(0%);
+    transition: 1s ease-in-out;
+    transition-delay: 500ms;
+  }
+
+  .container.sign-up-mode .inner-left {
+    transform: translate(-100%);
+    transition: 1s ease-in-out;
+    transition-delay: 500ms;
+  }
+
+  .container.sign-up-mode .login-container {
+    right: 50%;
+    transition: 1s ease-in-out;
+    transition-delay: 500ms;
+  }
+</style>
