a97d9135c5967d4701700c8df6c365780225dcc3
Breezy Frost <breezyfrost@163.com>
2023-12-08 23:18:02 +0800

    feat(menu): add menu and tab bar linkage

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="menu">
     <a-menu
+      v-model:open-keys="openKeys"
+      v-model:selected-keys="selectedKey"
       style="width: 220px; height: 100%"
       show-collapse-button
       :level-indent="34"
       breakpoint="xl"
+      auto-open-selected
     >
       <MenuTree :tree-data="treeData" />
     </a-menu>
@@ -13,7 +16,9 @@
 
 <script setup lang="ts">
   import { useAppStore } from '@/store';
-  import { computed } from 'vue';
+  import { computed, ref } from 'vue';
+  import { listenerRouteChange } from '@/utils/route-listener';
+  import { RouteRecordRaw } from 'vue-router';
   import MenuTree from './menu-tree.vue';
 
   const appStore = useAppStore();
@@ -21,6 +26,39 @@
   const treeData = computed(() => {
     return appStore.appAsyncMenus;
   });
+
+  const openKeys = ref<string[]>([]);
+  const selectedKey = ref<string[]>([]);
+
+  const findMenuOpenKeys = (target: string) => {
+    const result: string[] = [];
+    let isFind = false;
+    const backtrack = (item: RouteRecordRaw, keys: string[]) => {
+      if (item.name === target) {
+        isFind = true;
+        result.push(...keys);
+        return;
+      }
+      if (item.children?.length) {
+        item.children.forEach((el) => {
+          backtrack(el, [...keys, el.name as string]);
+        });
+      }
+    };
+    treeData.value.forEach((el: RouteRecordRaw) => {
+      if (isFind) return; // Performance optimization
+      backtrack(el, [el.name as string]);
+    });
+    return result;
+  };
+
+  listenerRouteChange((newRoute) => {
+    const menuOpenKeys = findMenuOpenKeys(newRoute.name as string);
+    console.log(menuOpenKeys);
+    const keySet = new Set([...menuOpenKeys, ...openKeys.value]);
+    openKeys.value = [...keySet];
+    selectedKey.value = [menuOpenKeys[menuOpenKeys.length - 1]];
+  }, true);
 </script>
 
 <style scoped>
